383348bfb3d245329458de7c7d7e331a4d524e7d
Hunter <124668150+Strat15@users.noreply.github.com>
2025-05-05 13:20:48 +1200

    Shell v1
    start of project only an outline

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+class Place:
+    def __init__(self, name, ):
+        self.name = name
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("The TODO List")
+    root.geometry("400x400")
+    root.config(bg="white")
